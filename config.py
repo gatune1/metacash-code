13c964211c0fa13fdad97814de92e025d0faba6e
Zack Gatune <gatunezack00@gmail.com>
2025-09-17 18:44:55 +0300

    Initial commit with Render deployment files

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey123'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///metacash.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+ECHO is on.
