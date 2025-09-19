eaff5150536f7b9a77f1eda575496f01fa3e0865
Zack Gatune <gatunezack00@gmail.com>
2025-09-19 17:09:34 +0300

    Add DATABASE_URL debug print in run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
+import os
 
 app = create_app()
+
+# Print DATABASE_URL to check which DB is being used
+print("DATABASE_URL being used:", os.getenv("DATABASE_URL"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
